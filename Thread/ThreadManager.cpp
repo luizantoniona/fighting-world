c607329bbe76b489f0366c82a3df886316b461c6
luizantoniona <luizantonio.na@hotmail.com>
2024-03-27 00:42:52 -0300

    feat> create method to retrieve thread by type and create thread

--- Thread/ThreadManager.cpp
+++ Thread/ThreadManager.cpp
@@ -1,9 +1,36 @@
 #include "threadmanager.h"
 
-ThreadManager::ThreadManager()
+#include <Thread/Database/ThreadDatabase.h>
+
+ThreadManager::ThreadManager() : _threads({})
 {
 }
 
 ThreadManager::~ThreadManager()
 {
+}
+
+void ThreadManager::create(const ThreadTypeEnum type)
+{
+    Thread *thread = threadByType(type);
+
+    if (!thread)
+    {
+        return;
+    }
+
+    _threads.at(type).insert(_threads.at(type).cend(), thread);
+}
+
+Thread *ThreadManager::threadByType(const ThreadTypeEnum type)
+{
+    switch (type)
+    {
+    case ThreadTypeEnum::THREAD_DATABASE:
+        return new ThreadDatabase();
+        break;
+    default:
+        return nullptr;
+        break;
+    }
 }
--- Thread/ThreadManager.h
+++ Thread/ThreadManager.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include <map>
+#include <list>
+
+#include <Thread/Thread.h>
+#include <Thread/ThreadTypeEnum.h>
+
 class ThreadManager
 {
-private:
 public:
     ThreadManager();
     ~ThreadManager();
+
+    void create(const ThreadTypeEnum type);
+
+private:
+    Thread *threadByType(const ThreadTypeEnum type);
+
+    std::map<ThreadTypeEnum, std::list<Thread *>> _threads;
 };
