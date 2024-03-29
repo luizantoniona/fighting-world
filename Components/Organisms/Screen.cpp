34c0beb6d56250355f7cd886a5e0b9ffb4e1c338
luizantoniona <luizantonio.na@hotmail.com>
2024-03-29 19:28:20 -0300

    feat: Create Screen class for specifc classes

---
+++ Components/Organisms/Screen.cpp
@@ -0,0 +1,15 @@
+#include "Screen.h"
+
+BEGIN_NAMESPACE_COMPONENT
+
+Screen::Screen()
+{
+}
+
+Screen::~Screen()
+{
+}
+
+void Screen::init() { const 0; }
+
+END_NAMESPACE_COMPONENT
---
+++ Components/Organisms/Screen.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Components/Components_Globals.h>
+
+BEGIN_NAMESPACE_COMPONENTS
+
+class Screen
+{
+public:
+    Screen();
+    ~Screen();
+
+    virtual void init();
+
+private:
+};
+
+END_NAMESPACE_COMPONENTS
