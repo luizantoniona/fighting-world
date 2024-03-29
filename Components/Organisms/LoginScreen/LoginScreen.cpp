046e75f870da05afd0ae71e8676ded7ab4451cc7
luizantoniona <luizantonio.na@hotmail.com>
2024-03-29 19:28:50 -0300

    feat: create Login Screen class

---
+++ Components/Organisms/LoginScreen/LoginScreen.cpp
@@ -0,0 +1,13 @@
+#include "LoginScreen.h"
+
+BEGIN_NAMESPACE_COMPONENT
+
+LoginScreen::LoginScreen()
+{
+}
+
+LoginScreen::~LoginScreen()
+{
+}
+
+END_NAMESPACE_COMPONENT
---
+++ Components/Organisms/LoginScreen/LoginScreen.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <Components/Components_Globals.h>
+
+#include <Components/Organisms/Screen.h>
+
+using namespace Component::Screen;
+
+BEGIN_NAMESPACE_COMPONENT
+
+class LoginScreen : public Screen
+{
+public:
+    LoginScreen();
+    ~LoginScreen();
+
+private:
+};
+
+END_NAMESPACE_COMPONENT
